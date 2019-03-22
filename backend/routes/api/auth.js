932d84b5d97cc2de6fc27c4bc6415b76182e9958
Dani Ortiz <dani.ortiz@devopensource.com>
2019-03-22 08:34:12 +0100

    Creada dos nuevas funciones para optimizar código repetido.

--- backend/routes/api/auth.js
+++ backend/routes/api/auth.js
@@ -1,75 +1,78 @@
 /* eslint-disable */
-let router = require('express').Router();
+const router = require('express').Router();
 const fetch = require('node-fetch');
+/* eslint-enable */
+
+tokenUser = async (userData) => {
+	return new Promise((resolve) => {
+		fetch('http://magento23pwa.test/index.php/rest/V1/integration/customer/token', {
+			method: 'POST',
+			body: JSON.stringify(userData),
+			headers: {
+				'Content-Type': 'application/json',
+			},
+		}).then((res) => res.json())
+			.then((response) => {
+				resolve(response);
+			});
+	});
+};
+
+infoUser = async (token) => {
+	return new Promise((resolve) => {
+		fetch('http://magento23pwa.test/index.php/rest/V1/customers/me', {
+			method: 'GET',
+			headers: {
+				'Authorization': 'Bearer '+ token,
+			},
+		}).then((res) => res.json())
+			.then((response) => {
+				if (response.message) {
+					resolve({error: response.message});
+				} else {
+					resolve({success: {user: response, token: token}});
+				}
+			});
+	});
+};
 
 router.post('/register', (req, res) => {
-    try {
-        fetch('http://magento23pwa.test/index.php/rest/V1/customers', {
-            method: 'POST',
-            body: JSON.stringify(req.body),
-            headers: {
-                'Content-Type': 'application/json',
-            },
-        }).then((res) => res.json())
-        .then((response) => {
-            if(response.message){
-                res.status(200).json({error: response.message})
-            }else {
-                let user = response;
-                fetch('http://magento23pwa.test/index.php/rest/V1/integration/customer/token', {
-                    method: 'POST',
-                    body: JSON.stringify({'username': req.body.customer.email, 'password': req.body.password}),
-                    headers: {
-                        'Content-Type': 'application/json',
-                    },
-                }).then((res) => res.json())
-                .then((response) => {
-                    res.status(200).json({success: {user: user, token: response}});
-                }).catch((error) => res.status(400).json({error: error}));
-            }
-        }).catch((error) => res.status(400).json({error: error}));
-    } catch (error) {
-        res.status(400).json({error: error});
-    }
+	try {
+		fetch('http://magento23pwa.test/index.php/rest/V1/customers', {
+			method: 'POST',
+			body: JSON.stringify(req.body),
+			headers: {
+				'Content-Type': 'application/json',
+			},
+		}).then((res) => res.json())
+			.then(async (response) => {
+				if (response.message) {
+					res.status(200).json({error: response.message});
+				} else {
+					const user = response;
+					const token = await tokenUser({
+						'username': req.body.customer.email,
+						'password': req.body.password,
+					});
+					res.status(200).json({success: {user: user, token: token}});
+				}
+			}).catch((error) => res.status(400).json({error: error}));
+	} catch (error) {
+		res.status(400).json({error: error});
+	}
 });
 
-router.post('/login', (req, res) => {
-    fetch('http://magento23pwa.test/index.php/rest/V1/integration/customer/token', {
-        method: 'POST',
-        body: JSON.stringify(req.body),
-        headers: {
-            'Content-Type': 'application/json',
-        },
-    }).then((res) => res.json())
-    .then((response) => {
-        if(response.message){
-            res.status(200).json({error: response.message})
-        }else {
-            let token = response;
-            fetch('http://magento23pwa.test/index.php/rest/V1/customers/me', {
-                method: 'GET',
-                headers: {
-                    'Authorization': 'Bearer '+token,
-                },
-            }).then((res) => res.json())
-            .then((response) => {
-                res.status(200).json({success: {user: response, token: token}})
-            }).catch((error) => res.status(400).json({error: error}))
-        }
-    }).catch((error) => res.status(400).json({error: error}))
+router.post('/login', async (req, res) => {
+	const token = await tokenUser(req.body);
+	if (token.message) {
+		res.status(200).json({error: token.message});
+	} else {
+		res.status(200).json(await infoUser(token));
+	}
 });
 
-router.post('/infouser', (req, res) => {
-    let token = req.body.token;
-    fetch('http://magento23pwa.test/index.php/rest/V1/customers/me', {
-        method: 'GET',
-        headers: {
-            'Authorization': 'Bearer '+ token,
-        },
-    }).then((res) => res.json())
-    .then((response) => {
-        res.status(200).json({success: {user: response, token: token}})
-    }).catch((error) => res.status(400).json({error: error}))
+router.post('/infouser', async (req, res) => {
+	res.status(200).json(await infoUser(req.body.token));
 });
 
 module.exports = router;
